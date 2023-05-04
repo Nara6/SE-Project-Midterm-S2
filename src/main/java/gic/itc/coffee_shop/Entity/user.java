9e6aedc5fab8f5a42a134e2c4b6d0a7f03943e49
YIN Soknara <yinnara6@gmail.com>
2023-05-04 10:15:49 +0700

    Model and Controller -User

--- src/main/java/gic/itc/coffee_shop/Entity/user.java
+++ src/main/java/gic/itc/coffee_shop/Entity/user.java
@@ -19,11 +19,53 @@ public class user {
 
     @Column(name = "username")
     private String username;
-
+    @Column
     private String password;
+    @Column
     private String email;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_type_id", referencedColumnName = "id")
     private user_type user_type;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public user_type getUser_type() {
+        return user_type;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUser_type(user_type user_type) {
+        this.user_type = user_type;
+    }
+    
 }
